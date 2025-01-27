37236e12d9eb870224c74246a17635c078554268
OmarStarlord <mr.freezenlm@gmail.com>
2025-01-27 01:13:20 +0100

    fixing db4

--- users/models.py
+++ users/models.py
@@ -2,8 +2,18 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    
-    
-    def get_full_name(self):
-        return f"{self.first_name} {self.last_name}".strip() or self.username
+    profile_pic_base64 = models.TextField(blank=True, null=True)  # Store Base64 string
+
+    def set_profile_pic(self, image_file):
+        import base64
+        from io import BytesIO
+        from PIL import Image
+        
+        # Convert image to base64
+        image = Image.open(image_file)
+        buffered = BytesIO()
+        image.save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
+        
+        self.profile_pic_base64 = img_str
+        self.save()
