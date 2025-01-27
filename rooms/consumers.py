5299a8d7622f97384a569ab70b2da4119741f5cc
OmarStarlord <mr.freezenlm@gmail.com>
2025-01-27 01:37:51 +0100

    fixing db8

--- rooms/consumers.py
+++ rooms/consumers.py
@@ -1,8 +1,18 @@
 import json
+import pusher
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth import get_user_model
 from django.db import close_old_connections
 from .models import Message, Room
+
+# Pusher client setup
+pusher_client = pusher.Pusher(
+    app_id='1931997',
+    key='b47d20482e4df2bf538c',
+    secret='e280f4c8b43ecfff83b4',
+    cluster='eu',
+    ssl=True
+)
 
 User = get_user_model()
 
@@ -12,14 +22,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
         self.room_group_name = f"chat_{self.room_id}"
 
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        # Accept WebSocket connection
         await self.accept()
 
     async def disconnect(self, close_code):
         """Handles WebSocket disconnection."""
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        pass  # No longer needed to handle group discard since Pusher is used
 
     async def receive(self, text_data):
+        """Handles the received message from WebSocket and sends it to Pusher."""
         data = json.loads(text_data)
         message_content = data["message"]
         sender_username = data["sender"]
@@ -30,19 +41,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
         if sender and room:
             profile_picture = sender.profile_pic.url if sender.profile_pic else "/static/images/default.png"
 
+            # Create the message and save it to the database
             message = await self.create_message(room, sender, message_content)
 
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    "type": "chat_message",
-                    "message": message_content,
-                    "sender": sender.username,
-                    "profile_picture": profile_picture
-                }
-            )
+            # Send the message to Pusher
+            pusher_client.trigger(self.room_group_name, 'new_message', {
+                'message': message_content,
+                'sender': sender.username,
+                'profile_picture': profile_picture
+            })
 
     async def chat_message(self, event):
+        """Handle incoming messages from Pusher and send them to the WebSocket."""
         await self.send(text_data=json.dumps({
             "message": event["message"],
             "sender": event["sender"],
